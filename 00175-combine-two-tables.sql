d3a534742336820f150742e01715f41c32448525
Jatin Aggarwal <121922356+Jatiinagarwal@users.noreply.github.com>
2026-03-17 14:08:54 +0530

    Refactor SQL join to use USING clause

--- 00175-combine-two-tables.sql
+++ 00175-combine-two-tables.sql
@@ -69,3 +69,9 @@ addressId = 1 contains information about the address of personId = 2.
 select firstName, lastName, city, state
 from Person p
 left join Address a on p.personId=a.personId
+
+
+select p.firstName, p.lastName, a.city, a.state
+from Person p
+left join Address a
+using(personId)
